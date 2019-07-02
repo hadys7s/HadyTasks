d2ed11e23fdec769550996eb5c68374584677d16
hadys7s <35876147+hadys7s@users.noreply.github.com>
2019-07-02 15:57:15 +0200

    mainactivity work

--- app/src/main/java/com/andalus/hady/MainActivity.java
+++ app/src/main/java/com/andalus/hady/MainActivity.java
@@ -1,7 +1,10 @@
 package com.andalus.hady;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        RecyclerView recyclerView = findViewById(R.id.Recycle1);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,3));
+        Data[] fake = new Data[]{
+                new Data("Youtube",70,R.drawable.youtube),
+                new Data("Instagram",60,R.drawable.instagram),
+                new Data("Itunes",50,R.drawable.itunes),
+                new Data("kik",70,R.drawable.kik),
+                new Data("Skype",55,R.drawable.skype),
+                new Data("Linkedin",70,R.drawable.linkedin),
+                new Data("Messenger",70,R.drawable.messenger),
+                new Data("Reddit",70,R.drawable.reddit),
+                new Data("Whatsapp",70,R.drawable.whatsapp),
+                new Data("Reddit",70,R.drawable.reddit),
+                new Data("Youtube",70,R.drawable.youtube),
+                new Data("Instagram",60,R.drawable.instagram),
+                new Data("Itunes",50,R.drawable.itunes),
+                new Data("kik",70,R.drawable.kik),
+                new Data("Skype",55,R.drawable.skype),
+                new Data("Linkedin",70,R.drawable.linkedin),
+                new Data("Messenger",70,R.drawable.messenger),
+                new Data("Reddit",70,R.drawable.reddit),
+                new Data("Whatsapp",70,R.drawable.whatsapp),
+                new Data("Reddit",70,R.drawable.reddit),
+
+
+
+
+        };
+
+        recyclerView.setAdapter(new Adapter(fake));
+
+
+
     }
 }
